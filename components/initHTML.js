f91d16cadb7d7305bfa8d6804f18e7211beae936
Eric Frey <ericlfrey@gmail.com>
2022-11-14 10:30:26 -0600

    Trying pphotos again

--- components/initHTML.js
+++ components/initHTML.js
@@ -1,5 +1,6 @@
 import renderToDom from '../utils/renderToDom';
 import jokeBtn from './jokeBtn';
+import logo from '../public/images/logo.svg';
 
 const initHTML = () => {
   const domStr = `
@@ -10,7 +11,11 @@ const initHTML = () => {
   <div id="jokeSetup"></div>
   <div id="jokePunchline"></div>
 `;
+  const footerStr = `
+  <img src=${logo} alt="" class="logo">
+    <p>2022</p>`;
   renderToDom('#app', domStr);
+  renderToDom('#footer', footerStr);
 };
 
 export default initHTML;
